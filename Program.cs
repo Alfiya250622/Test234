797227ffac71ed1d64aa44694a712bcb5a1f1390
Alfiya <al.abdrahmanova2010@yandex.ru>
2022-06-26 17:25:33 +0500

    Строка

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿//Дана строка. Подсчитать количество содержащихся в ней цифр
+
+//string newString = "125_tywerd_ef125we";
+
+string newString;
+newString = Console.ReadLine();
+if (newString != null)
+{
+    int count = 0;
+
+    foreach (var i in newString) if (char.IsDigit(i)) count++;
+    Console.WriteLine(count);
+}
